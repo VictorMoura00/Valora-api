0a8fc9677d220a6a4c7dc11bd76deb6aa461804c
Victor Moura <victormoura@proton.me>
2026-02-20 09:14:29 -0300

    refactor(entity): refactor entity base

--- Valora.Domain/Common/Abstractions/Entity.cs
+++ Valora.Domain/Common/Abstractions/Entity.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Valora.Domain.Common.Abstractions;
 
-public abstract class Entity
+public abstract class Entity : IEquatable<Entity>
 {
     public Guid Id { get; protected set; }
     public DateTimeOffset CreatedAt { get; protected set; }
@@ -16,7 +16,7 @@ public abstract class Entity
         IsDeleted = false;
     }
 
-    public void SetUpdated()
+    protected void SetUpdated()
     {
         UpdatedAt = DateTimeOffset.UtcNow;
     }
@@ -26,5 +26,15 @@ public abstract class Entity
         IsDeleted = true;
         SetUpdated();
     }
-    
+
+    public bool Equals(Entity? other)
+    {
+        if (other is null) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return Id == other.Id;
+    }
+
+    public override bool Equals(object? obj) => Equals(obj as Entity);
+
+    public override int GetHashCode() => Id.GetHashCode();
 }
